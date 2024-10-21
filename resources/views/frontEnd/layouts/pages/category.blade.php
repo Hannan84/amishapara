07d71a714f1d5969defc049872a2c12b2b119c28
basudev-g <flancer.basudev@gmail.com>
2024-10-21 11:38:22 +0600

    style change for add to cart

--- resources/views/frontEnd/layouts/pages/category.blade.php
+++ resources/views/frontEnd/layouts/pages/category.blade.php
@@ -322,7 +322,7 @@
     </script>
     <script>
         $(".cart-badge").click(function() {
-            $(".product_item_inner").css("opacity", "0.7");
+            $(".product_item_inner").addClass("product-overflow-quantity show");
             // alert("hello");
             // $(".product_item").addClass("product-overflow-quantity show");
 
